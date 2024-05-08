f9133622eba8e606c97739f9897a021027d0f57f
S120916\최재영 <cjy2103@naver.com>
2024-05-08 16:45:54 +0900

    플러터 텍스트 예제 (메이플 볼드, 라이트체 적용 완)

--- basic/text/lib/main.dart
+++ basic/text/lib/main.dart
@@ -10,11 +10,42 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  const MaterialApp(
-      home: Padding(
-          padding: EdgeInsets.only(top:  40.0, left: 50.0),
-        child: Text("하이")
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body : Padding(padding: EdgeInsets.only(top:  40.0, left: 20.0),
+            child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("하이"),
+              SizedBox(height: 10),
+              Text("Hello", style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.normal,
+                decoration: TextDecoration.none,
+                color: Colors.black
+                )
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Text("메이플 볼드체", style: TextStyle(
+                    fontSize: 22,
+                    fontFamily: 'MapleStoryBold'
+                    )
+                  ),
+                  SizedBox(width: 10),
+                  Text("메이플 라이트체", style: TextStyle(
+                    fontSize: 22,
+                    fontFamily: 'MapleStoryLight'
+                    )
+                  )
+                ],
+              )
+              
+            ],
+          )
+        )
       )
-
     );
   }
 }

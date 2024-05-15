265aaefeb152c0c7b631670f1591c94d7e0411ae
jy.choi <cjy2103@naver.com>
2024-05-15 21:22:46 +0900

    하이퍼링크 넣기 준비

--- basic/screen_move/lib/main.dart
+++ basic/screen_move/lib/main.dart
@@ -9,10 +9,38 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            children: [
+              SizedBox(height: 80.0),
+
+              Image.asset(
+                'assets/images/tamtam.jpg',
+                width: 200,
+                height: 200,
+              ),
+                Text("탬탬버린"),
+                SizedBox(height: 10.0),
+                Text("유튜브 주소 : https://www.youtube.com/@chamcham2"),
+                SizedBox(height: 30.0),
+
+              OutlinedButton( onPressed: () {
+                print("OutlinedButton 클릭됨");
+              },
+                child: Text(
+                    "이쭈냥이오"
+                    , style: TextStyle(color: Colors.black)),
+                style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Colors.pink)
+                ),
+
+              ),
+
+
+            ],
+          ),
         ),
       ),
     );

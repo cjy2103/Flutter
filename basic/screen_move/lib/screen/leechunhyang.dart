ec6b31bf2c7df93100e0be48681b0bdc03e8af20
S120916\최재영 <cjy2103@naver.com>
2024-05-16 17:54:14 +0900

    화면이동 기능 추가

---
+++ basic/screen_move/lib/screen/leechunhyang.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class LeeChunhyangScreen extends StatelessWidget {
+  const LeeChunhyangScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Text('This is the new screen'),
+        ),
+    );
+  }
+}
---
+++ basic/screen_move/lib/screen/tamtam.dart
@@ -0,0 +1,75 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'leechunhyang.dart';
+
+class TamTamScreen extends StatelessWidget {
+  const TamTamScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 80.0),
+
+            Image.asset(
+              'assets/images/tamtam.jpg',
+              width: 200,
+              height: 200,
+            ),
+            Text("탬탬버린"),
+            SizedBox(height: 10.0),
+            RichText(
+              text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '유튜브 주소: ',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  TextSpan(
+                      text: 'https://www.youtube.com/@chamcham2',
+                      style: TextStyle(color: Colors.blue, decoration: TextDecoration.underline),
+                      recognizer: TapGestureRecognizer()..onTap = _launchYouTubeChannel
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 30.0),
+
+            OutlinedButton( onPressed: () {
+              // 나중에 화면 이동예제 쓸것.
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LeeChunhyangScreen()),
+              );
+            },
+              child: Text(
+                  "이쭈냥이오"
+                  , style: TextStyle(color: Colors.black)),
+              style: OutlinedButton.styleFrom(
+                  side: BorderSide(color: Colors.pink)
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _launchYouTubeChannel() async {
+    const url = 'https://www.youtube.com/@chamcham2';
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {
+      // Handle the case where the URL can't be launched
+      print('Could not launch YouTube channel URL');
+    }
+  }
+}

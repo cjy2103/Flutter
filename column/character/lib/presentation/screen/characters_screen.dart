77076a16c26f22e9e495b43e2dab2cec1d9faf10
S120916\최재영 <cjy2103@naver.com>
2024-05-21 18:04:50 +0900

    리스트뷰 Item 요소 생성완료 디테일한 부분 다시 잡기

--- column/character/lib/presentation/screen/characters_screen.dart
+++ column/character/lib/presentation/screen/characters_screen.dart
@@ -29,14 +29,48 @@ Widget _buildList() => ListView(
   children: [
     _character('탬탬버린','힐링캠프','assets/images/tamtam.jpg')
 
+
   ],
   shrinkWrap: true, // 콘텐츠 높이에 맞게 감싸짐
 );
 
-ListTile _character(String title, String subtitle, String img) => ListTile(
-    title: Text(title),
-    subtitle: Text(subtitle),
-    leading: Image.asset(img)
-);
+Widget _character(String title, String subtitle, String img) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Row(
+      children: [
+        Image.asset(
+          img,
+          width: 100,
+          height: 100,
+          fit: BoxFit.cover,
+        ),
 
+        SizedBox(width: 10), // 이미지와 텍스트 사이의 간격을 위한 SizedBox
 
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                subtitle,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey[600],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
+}

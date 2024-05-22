b29a217c94996086b4be81caa5877187b10118ac
S120916\최재영 <cjy2103@naver.com>
2024-05-22 15:30:17 +0900

    데이터 추가/ 제거 기능 템플릿 준비

---
+++ column/character_add/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:character_add/presentation/screen/characters_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+            child: CharacterScreen()
+        ),
+      ),
+    );
+  }
+}
---
+++ column/character_add/lib/presentation/screen/characters_screen.dart
@@ -0,0 +1,135 @@
+
+import 'package:flutter/material.dart';
+
+const imagePath = 'assets/images/';
+
+class CharacterScreen extends StatelessWidget {
+  const CharacterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+        body: Center(
+          child: Column(
+              children: [
+                SizedBox(height: 60.0),
+
+                Text("List 예제"),
+                
+                SizedBox(height: 40.0),
+
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 20.0),
+                    child: _insertButton(),
+                  ),
+                ),
+
+                SizedBox(height: 20.0),
+
+                _buildList()
+              ],
+          ),
+        ),
+      );
+  }
+
+}
+
+Widget _insertButton() => InkWell(
+    onTap: () {
+       //TODO : ListVIew에 아이템을 추가해!
+    },
+    child: CircleAvatar(
+      radius: 30.0,
+      backgroundColor: Colors.blue,
+      child: Icon(
+        Icons.add,
+        color: Colors.white,
+      ),
+    )
+);
+
+
+
+Widget _buildList() => Expanded(
+
+  child: ListView(
+      padding: EdgeInsets.zero,
+      children: [
+        _character('탬탬버린','힐링캠프',imagePath + 'tamtam.jpg'),
+        _character('이춘향', '힐링캠프', imagePath + 'leechunhyang.jpg'),
+        _character('마뫄','힐링캠프',imagePath + 'mwama.jpg'),
+        _character('삐부', '힐링캠프', imagePath + 'bbibu.webp'),
+      ],
+    ),
+);
+
+Widget _character(String title, String subtitle, String img) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 20.0), // 수평 방향으로 패딩 추가
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: const Color(0xFFBABABA), // Row의 테두리 색상
+          width: 1.0, // Row의 테두리 두께
+        ),
+
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(0.0), // 내부 패딩 추가
+        child: Row(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: const Color(0xFFBABABA), // 이미지의 테두리 색상
+                  width: 1.0, // 이미지의 테두리 두께
+                ),
+
+              ),
+              child: ClipRRect(
+                child: Image.asset(
+                  img,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Text(
+                      subtitle,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}

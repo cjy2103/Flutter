eb67af0227fdfe31f317d28df3668faa5f0a1c0a
S120916\최재영 <cjy2103@naver.com>
2024-05-22 13:47:49 +0900

    플러터(Flutter) 기초 - ListView(리스트 뷰) ( v )

--- column/character/lib/presentation/screen/characters_screen.dart
+++ column/character/lib/presentation/screen/characters_screen.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 
+const imagePath = 'assets/images/';
+
 class CharacterScreen extends StatelessWidget {
   const CharacterScreen({super.key});
 
@@ -25,52 +27,89 @@ class CharacterScreen extends StatelessWidget {
 
 }
 
-Widget _buildList() => ListView(
-  children: [
-    _character('탬탬버린','힐링캠프','assets/images/tamtam.jpg')
-
-
-  ],
-  shrinkWrap: true, // 콘텐츠 높이에 맞게 감싸짐
+Widget _buildList() => Expanded(
+    child: ListView(
+      children: [
+        _character('탬탬버린','힐링캠프',imagePath + 'tamtam.jpg'),
+        _character('이춘향', '힐링캠프', imagePath + 'leechunhyang.jpg'),
+        _character('마뫄','힐링캠프',imagePath + 'mwama.jpg'),
+        _character('삐부', '힐링캠프', imagePath + 'bbibu.webp'),
+        _character('탬탬버린','힐링캠프',imagePath + 'tamtam.jpg'),
+        _character('이춘향', '힐링캠프', imagePath + 'leechunhyang.jpg'),
+        _character('마뫄','힐링캠프',imagePath + 'mwama.jpg'),
+        _character('삐부', '힐링캠프', imagePath + 'bbibu.webp'),
+        _character('탬탬버린','힐링캠프',imagePath + 'tamtam.jpg'),
+        _character('이춘향', '힐링캠프', imagePath + 'leechunhyang.jpg'),
+        _character('마뫄','힐링캠프',imagePath + 'mwama.jpg'),
+        _character('삐부', '힐링캠프', imagePath + 'bbibu.webp'),
+      ],
+    ),
 );
 
 Widget _character(String title, String subtitle, String img) {
   return Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: Row(
-      children: [
-        Image.asset(
-          img,
-          width: 100,
-          height: 100,
-          fit: BoxFit.cover,
+    padding: const EdgeInsets.symmetric(horizontal: 20.0), // 수평 방향으로 패딩 추가
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: const Color(0xFFBABABA), // Row의 테두리 색상
+          width: 1.0, // Row의 테두리 두께
         ),
 
-        SizedBox(width: 10), // 이미지와 텍스트 사이의 간격을 위한 SizedBox
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(0.0), // 내부 패딩 추가
+        child: Row(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: const Color(0xFFBABABA), // 이미지의 테두리 색상
+                  width: 1.0, // 이미지의 테두리 두께
+                ),
 
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
+              ),
+              child: ClipRRect(
+                child: Image.asset(
+                  img,
+                  fit: BoxFit.cover,
                 ),
               ),
-              SizedBox(height: 5),
-              Text(
-                subtitle,
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.grey[600],
-                ),
+            ),
+
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Text(
+                      subtitle,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      ],
+      ),
     ),
   );
 }

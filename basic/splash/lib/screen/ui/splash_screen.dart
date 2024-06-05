855404f73c17fd30bee1d58ff7d70b9f20c1dad2
jy.choi2 <cjy2103@naver.com>
2024-06-05 17:55:08 +0900

    플러터(Flutter) 기초 - Splash 화면 만들기  ( v )

--- basic/splash/lib/screen/ui/splash_screen.dart
+++ basic/splash/lib/screen/ui/splash_screen.dart
@@ -1,7 +1,4 @@
-
 import 'dart:async';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:splash/screen/ui/home_screen.dart';
 
@@ -13,24 +10,10 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
-  AnimationController? _controller;
-  Animation<double>? _animation;
 
     @override
     void initState() {
       super.initState();
-
-      // // 애니메이션 컨트롤러 설정
-      // _controller = AnimationController(
-      //   duration: const Duration(seconds: 1),
-      //   vsync: this,
-      // )
-      //   ..repeat(reverse: true);
-      //
-      // _animation = CurvedAnimation(
-      //   parent: _controller!,
-      //   curve: Curves.easeIn,
-      // );
 
       // 타이머 설정
       Timer(Duration(seconds: 1), () {
@@ -40,20 +23,10 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
     }
 
     @override
-    void dispose() {
-      _controller?.dispose();
-      super.dispose();
-    }
-
-    @override
     Widget build(BuildContext context) {
       return Scaffold(
         body: Center(
           child: Image.asset('assets/images/tamtam.jpg'),
-          // child: FadeTransition(
-          //   opacity: _animation!,
-          //   child: Image.asset('assets/images/tamtam.jpg'), // 로고 이미지 경로
-          // ),
         ),
       );
     }

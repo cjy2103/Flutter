a0b08032e7fceb050871e808fa81a8919c50f901
jy.choi2 <cjy2103@naver.com>
2024-06-13 14:23:59 +0900

    TextField 테두리색 변경전 코드 저장

--- basic/text_filed/lib/main.dart
+++ basic/text_filed/lib/main.dart
@@ -3,61 +3,101 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MainApp());
 }
-
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final TextEditingController emailEditingController = TextEditingController();
+  final TextEditingController passwordEditingController = TextEditingController();
+  String password = '';
+
+  @override
+  void initState() {
+    super.initState();
+    emailEditingController.addListener(() {
+      setState(() {});
+    });
+
+    passwordEditingController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    emailEditingController.dispose();
+    passwordEditingController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final TextEditingController emailEditingController = TextEditingController();
-    final TextEditingController passwordEditingController = TextEditingController();
-
     return MaterialApp(
       home: Scaffold(
         body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(height: 100.0),
-
+              const SizedBox(height: 100.0),
               SizedBox(
                 width: 300.0,
                 child: TextField(
                   controller: emailEditingController,
-                  decoration: const InputDecoration(
+                  decoration: InputDecoration(
                     border: OutlineInputBorder(),
-                    labelText: 'email',
-                    prefixIcon: Icon(Icons.email)
+                    labelText: 'Email',
+                    prefixIcon: Icon(Icons.email),
+                    suffixIcon: emailEditingController.text.isNotEmpty
+                        ? IconButton(
+                      icon: const Icon(Icons.cancel),
+                      onPressed: () {
+                        emailEditingController.clear();
+                      },
+                    )
+                        : null,
+                    
                   ),
                 ),
               ),
-
-              SizedBox(height: 50.0,),
-
+              const SizedBox(height: 50.0),
               SizedBox(
                 width: 300.0,
                 child: TextField(
                   controller: passwordEditingController,
                   obscureText: true,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'password',
-                      prefixIcon: Icon(Icons.lock)
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    labelText: 'Password',
+                    prefixIcon: const Icon(Icons.lock),
+                    suffixIcon: passwordEditingController.text.isNotEmpty
+                        ? IconButton(
+                      icon: const Icon(Icons.cancel),
+                      onPressed: () {
+                        passwordEditingController.clear();
+                      },
+                    )
+                        : null,
                   ),
                 ),
               ),
-
-              SizedBox(height: 50.0,),
-
+              const SizedBox(height: 50.0),
               ElevatedButton(
                 onPressed: () {
-                  // 버튼이 눌리면 입력된 텍스트를 출력합니다.
-                  print('Entered text: ${emailEditingController.text}');
+                  // 버튼이 눌리면 입력된 패스워드를 Text 위젯에 전달
+                  setState(() {
+                    password = passwordEditingController.text;
+                  });
                 },
                 child: const Text('Submit'),
               ),
+              const SizedBox(height: 50.0),
+              Text('Password: $password'),
             ],
-          )
+          ),
         ),
       ),
     );

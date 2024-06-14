ede9412fac979b03f8c6f7ca9e30cbfd3222d13b
jy.choi2 <cjy2103@naver.com>
2024-06-14 16:41:10 +0900

    Flutter Dialog 준비

--- basic/permission/lib/main.dart
+++ basic/permission/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MainApp());
@@ -6,6 +10,35 @@ void main() {
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
+
+  Future<void> requestStoragePermission() async {
+    if (Platform.isAndroid && await _isAndroid13OrAbove()) {
+      // Android 13 이상
+      PermissionStatus imagesStatus = await Permission.photos.request();
+      PermissionStatus videoStatus = await Permission.videos.request();
+      PermissionStatus audioStatus = await Permission.audio.request();
+
+      if (imagesStatus.isGranted && videoStatus.isGranted && audioStatus.isGranted) {
+        print("저장소 권한이 허용되었습니다.");
+      } else {
+        print("저장소 권한이 거부되었습니다.");
+      }
+    } else {
+      // Android 12 이하 및 다른 플랫폼
+      PermissionStatus status = await Permission.storage.request();
+      if (status.isGranted) {
+        print("저장소 권한이 허용되었습니다.");
+      } else {
+        print("저장소 권한이 거부되었습니다.");
+      }
+    }
+  }
+
+  Future<bool> _isAndroid13OrAbove() async {
+    final androidInfo = await DeviceInfoPlugin().androidInfo;
+    return androidInfo.version.sdkInt >= 33;
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +50,8 @@ class MainApp extends StatelessWidget {
               children: [
               const SizedBox(height: 80.0),
 
-              ElevatedButton(onPressed: (){
-                  // 저장소 권한 요청
+              ElevatedButton(onPressed: () async {
+                  await requestStoragePermission();
               }, child: const Text("저장소 권한 요청",
                 style: TextStyle(color: Colors.black),)
                 ,
